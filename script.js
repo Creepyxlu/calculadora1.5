a74923872b0597e0f470af43b41ddf0baeaa1153
Lu <eyelenm@outlook.com>
2024-08-05 19:18:25 -0300

    aodksfgjasodf

--- script.js
+++ script.js
@@ -12,31 +12,39 @@ numero1 = 1
 numero2 = 2
 operacion= '+' */
 
-let numero = Number(prompt("Ingrese un número"))
+let numero = prompt("Ingrese un número")
 
 
-while (isNaN(numero) || numero == null || numero == ""){
+while (isNaN(numero) || !numero){
     numero = prompt("Por favor, ingrese un número :/")
 }
 
 let numero2 = prompt ("Ingrese otro número")
 
-while (isNaN(numero2) || numero2 == null || numero2 == ""){
+while (isNaN(numero2) || !numero2){
     numero2 = prompt("Por favor, ingrese otro número :(")
 }
 
-let simbolo = prompt ("Ingrese uno de los siguientes simbolos \n ('+', '-', '*', '/')")
+numero = Number(numero)
+numero2 = Number(numero2)
 
+let simbolo = prompt ("Ingrese uno de los siguientes simbolos \n ('+', '-', '*', '/') ")
+
+while(simbolo != "+" && simbolo != "-" && simbolo != "*" && simbolo != "/"){
+    simbolo = prompt ("Por favor ingrese uno de los siguientes simbolos \n ('+', '-', '*', '/') ")
+}
 if (simbolo === "+"){
     let resultadoSuma = numero + numero2
-    alert("El resultado de la suma es "+ resultadoSuma)
+    alert("El resultado de la suma es " + resultadoSuma)
 }else if(simbolo === "-"){
     let resultadoResta = numero - numero2
     alert("El resultado de la resta es "+ resultadoResta)
 }else if(simbolo === "*"){
     let resultadoMultiplicacion = numero * numero2
     alert("El resultado de la multiplicación es "+ resultadoMultiplicacion)
-}else if(simbolo == "/"){
+}else if(simbolo === "/"){
     let resultadoDivision = numero / numero2
     alert("El resultado de la división es " + resultadoDivision)
+}else{
+
 }
